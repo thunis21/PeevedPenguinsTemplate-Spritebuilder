74f32708ab57a034a9f175c5b5613fea7b4a60cc
Thunis <thuniskruger@gmail.com>
2015-01-06 19:49:49 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -13,5 +13,7 @@
 
 -(void)play{
     CCLOG(@"Play button pressed");
+    CCScene *gameplayScene=[CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 @end
